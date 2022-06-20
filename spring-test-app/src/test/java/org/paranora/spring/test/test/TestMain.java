570cf034224cf65b5262a3d1c9c7395e29d561a7
paranora <yangqun@centaline.com.cn>
2022-06-20 13:50:46 +0800

    u

--- spring-test-app/src/test/java/org/paranora/spring/test/test/TestMain.java
+++ spring-test-app/src/test/java/org/paranora/spring/test/test/TestMain.java
@@ -16,22 +16,21 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, QuartzAutoConfiguration.class})
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {TestMain.class})
-@ActiveProfiles({"development"})
+@ActiveProfiles({"development","jedis"})
 @Import({})
 public class TestMain {
 
-//    @Autowired
-//    private CacheManager cacheManager;
+    @Autowired
+    private CacheManager cacheManager;
 
     @Test
     public void testA() throws Exception {
-//        String key = "paranora-test";
-//        String cacheBucket="cache-bucket1";
-//        Cache cache = cacheManager.getCache(cacheBucket);
-//        cache.put(key, "hello, i am paranora.");
-//
-//        Cache.ValueWrapper v = cache.get(key);
+        String key = "paranora-test";
+        String cacheBucket="cache-bucket1";
+        Cache cache = cacheManager.getCache(cacheBucket);
+        cache.put(key, "hello, i am paranora.");
 
+        Cache.ValueWrapper v = cache.get(key);
         System.out.println("end");
     }
 
